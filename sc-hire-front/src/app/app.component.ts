9485b249ac9a24b829f6de16625c5398d781d375
Jo Ran <jovanarandjelovic992@gmail.com>
2021-11-15 17:58:22 +0100

    Remove unnecessary files

--- sc-hire-front/src/app/app.component.ts
+++ sc-hire-front/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'sc-hire-front';
-}
+export class AppComponent {}
