35cec53e243771938bdd8cb68934d417366ccfe6
Jo Ran <jovanarandjelovic992@gmail.com>
2021-11-16 18:07:54 +0100

    Generate wrapper Auth component
    Generate Auth Module/Auth Routing

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sc-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
